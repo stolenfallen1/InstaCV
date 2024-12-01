69cd5268e128a027254ea7d495dfb37dae66c3cf
stolenfallen1 <jquizeo_ccs@uspf.edu.ph>
2024-12-02 07:00:14 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// import { useState } from 'react'
+
+function App() {
+  return (
+    <>
+      <div className="flex items-center justify-center">
+        <h1>HELLO RESUME BUILDER</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
