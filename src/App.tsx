a4352589a52f57ae18ea94b30d776bb29a9cb8ff
stolenfallen1 <jquizeo_ccs@uspf.edu.ph>
2024-12-02 07:17:53 +0800

    dark mode set-up and shadcn-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 // import { useState } from 'react'
+import { ThemeProvider } from './components/theme-provider.tsx'
+import { ModeToggle } from "./components/mode-toggle"
 
 function App() {
   return (
-    <>
+    <ThemeProvider defaultTheme='system' storageKey='vite-ui-theme'>
       <div className="flex items-center justify-center">
         <h1>HELLO RESUME BUILDER</h1>
+        <ModeToggle />
       </div>
-    </>
+    </ThemeProvider>
   )
 }
 
